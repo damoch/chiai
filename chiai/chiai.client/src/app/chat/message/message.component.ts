370c3fa04af1496fd5321ea5564cb2a0ae61036d
Damian Ochramowicz <damochramowicz260@gmail.com>
2025-02-18 20:23:38 +0100

    Styles cleanup

--- chiai/chiai.client/src/app/chat/message/message.component.ts
+++ chiai/chiai.client/src/app/chat/message/message.component.ts
@@ -9,4 +9,11 @@ import { Component, Input } from '@angular/core';
 export class MessageComponent {
   @Input() user!: string;
   @Input() text!: string;
+
+  aiLiked: boolean | null = null;
+
+  giveFeedback(like: boolean) {
+    this.aiLiked = like;
+    console.log(`User ${like ? 'liked' : 'disliked'} the AI response: "${this.text}"`);
+  }
 }
